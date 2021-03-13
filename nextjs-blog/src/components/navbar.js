092c9f986f7f1fd86185c2ef661e4c6a2aa39e5f
JDParra1366757 <80284239+JDParra1366757@users.noreply.github.com>
2021-03-13 16:45:48 -0600

    final commit

---
+++ nextjs-blog/src/components/navbar.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Nav = styled.nav'
+height: 80px 
+background: #000
+'
+
+const navbar = () => {
+    return (
+        <Nav>
+            <h1>
+                Navbar
+            </h1>
+        </Nav>
+    )
+}
+
+export default navbar
