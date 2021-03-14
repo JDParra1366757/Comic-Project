1dc9c4bd15cd406db04f4d3824d09b12e44d0746
JDParra1366757 <80284239+JDParra1366757@users.noreply.github.com>
2021-03-14 17:00:57 -0600

    archivecomplete

--- nextjs-blog/pages/archive.js
+++ nextjs-blog/pages/archive.js
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 
 const Hero = styled.div`
   height: 90vh;
+  overflow: scroll;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,8 +18,10 @@ const Hero = styled.div`
 //4000*2000 parecer ser mas del doble y el circulo se apachurra
 
 const Heading = styled.h1`
-  color: #000;
-  font-size: 10rem;
+  color: white;
+  padding: 10px 30px;
+  font-size: 4rem;
+  text-align: center;
   font-weight: 900;
 `
 
@@ -30,7 +33,32 @@ export default function Home() {
         <link rel="icon" href="/icon.jpg" />
       </Head>
       <Hero>
+          <div className="container">
+            <div className="grid">
+            <Heading>Chapter 1</Heading>
+                <a href="/page1" className="card1a">
+                    <h3>page1</h3>
+                </a>
 
+                <a href="/page2" className="card2a">
+                    <h3>page2</h3>
+                </a>
+
+                <a href="/prevpage" className="cardpreva">
+                    <h3>page3</h3>
+                </a>
+
+                <a href="/" className="cardfinala">
+                    <h3>latest</h3>
+                </a>
+        </div>
+        <div className="grid">
+            <Heading>Chapter 2</Heading>
+                <a href="/page1" className="cardcom">
+                    <h3>soon</h3>
+                </a>
+        </div>
+            </div>
       </Hero>
       <Footeralt />
     </>
