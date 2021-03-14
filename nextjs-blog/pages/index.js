fceb24451ede8a1aa0840a7f2301f9326f04d52d
JDParra1366757 <80284239+JDParra1366757@users.noreply.github.com>
2021-03-13 21:14:55 -0600

    links

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -5,12 +5,12 @@ import Navbar from 'src/components/navbar'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Demonic Coompendium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-    </div>
+    </>
       )
 }
--- nextjs-blog/src/components/navbar.js
+++ nextjs-blog/src/components/navbar.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'next/link'
 
 const Nav = styled.nav` 
     height: 80px;
     background: #000; 
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `
+const StyledLink = styled.a`
+    padding: 0rem 2rem;
+`
+
 
 
 const Navbar = () => {
     return (
         <Nav>
-            <h1>
-                Navbar
-            </h1>
+            <div>
+               <Link  href="/" passHref>
+                    <StyledLink>NXT</StyledLink>
+               </Link>
+            </div>
+            <div>
+               <Link  href="/persona3" passHref>
+                    <StyledLink>Persona 3</StyledLink>
+               </Link>
+               <Link  href="/persona4" passHref>
+                    <StyledLink>Persona 4</StyledLink>
+               </Link>
+               <Link  href="/persona5" passHref>
+                    <StyledLink>Persona 5</StyledLink>
+               </Link>
+            </div>
         </Nav>
     );
 };
