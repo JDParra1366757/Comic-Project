8fd36dcb766191685538881954904006b57213b1
JDParra1366757 <80284239+JDParra1366757@users.noreply.github.com>
2021-04-05 13:28:47 -0500

    restart

---
+++ api/routes/comics.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get("/", function(req, res, next)
+    {
+        res.json(
+            
+            {
+                "comics": {
+                  "data": [
+                    {"author":"Juan Daniel",
+                    "numPages":4,
+                    "description":"This comic is about....",
+                    "imageFile":"http://drive.google.comIdsasdassdad"
+                }
+                  ]
+                }
+            }
+        
+        
+        
+        
+        );
+    }
+);
+
+
+module.exports = router;
